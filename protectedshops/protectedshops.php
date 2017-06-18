57b707f088dcd5ddec6e319a36cfea94d8263e15
Katrin <ashly12323@gmail.com>
2017-06-18 14:02:10 +0300

    Create dbtable on plugin init

--- protectedshops/protectedshops.php
+++ protectedshops/protectedshops.php
@@ -1,11 +1,24 @@
 <?php
+
 /*
 Plugin Name: ProtectedShops
 */
 
-register_activation_hook( __FILE__, 'protectedshops_activate' );
+add_action( 'init', 'activate');
 
 function activate()
 {
-    echo "I am active"; exit;
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'ps_shops';
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        ID INT NOT NULL AUTO_INCREMENT,
+        shopId varchar(32) NOT NULL,
+        wp_userID INT NOT NULL,
+        PRIMARY KEY id (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
